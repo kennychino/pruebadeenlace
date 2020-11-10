1aac7404fbb9b1a100ae61eefd5ee0b4f7ff1ed1
raul <kenny_1_6@hotmail.com>
2020-11-10 11:29:07 -0500

    Empezamos probando con un simple codigo

---
+++ Holamundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola amingos esto es una prueba");
+        }
+    }
+}
